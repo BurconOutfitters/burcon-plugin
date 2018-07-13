6d17a4c70beaf3705b40676e5b4d1c13136800a1
Greg Sweet <greg@ccdzine.com>
2018-07-13 14:09:30 -0800

    Dashboard
    Changed some of the code from the starter theme.

--- admin/dashboard/partials/welcome-panel.php
+++ admin/dashboard/partials/welcome-panel.php
@@ -26,26 +26,29 @@ $user_name    = $current_user->display_name;
 
 // Add a filterable subheading.
 $subheading = sprintf(
-	'<h3>%1s %2s %3s.</h3>',
-	esc_html__( 'This is your custom', 'burcon-plugin' ),
-	get_bloginfo( 'name' ),
-	esc_html__( 'welcome panel', 'burcon-plugin' )
+	'<h3>%1s</h3>',
+	esc_html__( 'Here are some quick links to start editing content&hellip;', 'burcon-plugin' )
 );
 $subheading = apply_filters( 'burcon_welcome_subheading', $subheading );
 
+// Add a filterable description.
+$about_desc = apply_filters( 'burcon_welcome_about', __( 'Put your welcome message here.', 'burcon-plugin' ) );
+
 ?>
 <?php do_action( 'burcon_before_welcome_panel_content' ); ?>
-<div class="welcome-panel-content">
+<div class="welcome-panel-content custom">
 	<?php do_action( 'burcon_welcome_panel_content_before' ); ?>
 
 	<?php echo sprintf(
 		'<h2>%1s %2s.</h2>',
-		esc_html__( 'Welcome,', 'burcon-plugin' ),
+		esc_html__( 'Howdy,', 'burcon-plugin' ),
 		$user_name
 	); ?>
-	<p class="about-description"><?php _e( 'Put your welcome message here.' ); ?></p>
+	<p class="about-description"><?php echo $about_desc; ?></p>
+	<div class="dashboard-summary">
+		<?php wp_dashboard_right_now(); ?>
+	</div>
 	<?php echo $subheading; ?>
-	<p><?php _e( 'Use this to provide handy links to manage content, informational widgets, or maybe an instructional video.' ); ?></p>
 
 	<div class="welcome-panel-column-container">
 		<?php do_action( 'burcon_welcome_panel_column_container_before' ); ?>
